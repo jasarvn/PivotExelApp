ac205ab62693e795668d3b49e138bddb8ddeed79
JASPER ARVIN L. ABELLA <JASARVN@GMAIL.COM>
2020-02-29 02:18:11 +0800

    jasper_20200229_update_home

--- pivotExelApp/pivotExelApp/Form1.Designer.vb
+++ pivotExelApp/pivotExelApp/Form1.Designer.vb
@@ -22,10 +22,104 @@ Partial Class Form1
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.txtRawData = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.txtReferenceData = New System.Windows.Forms.TextBox()
+        Me.btnSelectRaw = New System.Windows.Forms.Button()
+        Me.btnSelectReference = New System.Windows.Forms.Button()
+        Me.btnCreatePivot = New System.Windows.Forms.Button()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'txtRawData
+        '
+        Me.txtRawData.Location = New System.Drawing.Point(28, 45)
+        Me.txtRawData.Name = "txtRawData"
+        Me.txtRawData.Size = New System.Drawing.Size(349, 20)
+        Me.txtRawData.TabIndex = 0
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.ForeColor = System.Drawing.Color.DimGray
+        Me.Label1.Location = New System.Drawing.Point(12, 9)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(162, 20)
+        Me.Label1.TabIndex = 1
+        Me.Label1.Text = "Pivot Table Creator"
+        '
+        'txtReferenceData
+        '
+        Me.txtReferenceData.Location = New System.Drawing.Point(28, 71)
+        Me.txtReferenceData.Name = "txtReferenceData"
+        Me.txtReferenceData.Size = New System.Drawing.Size(349, 20)
+        Me.txtReferenceData.TabIndex = 2
+        '
+        'btnSelectRaw
+        '
+        Me.btnSelectRaw.Location = New System.Drawing.Point(383, 42)
+        Me.btnSelectRaw.Name = "btnSelectRaw"
+        Me.btnSelectRaw.Size = New System.Drawing.Size(143, 23)
+        Me.btnSelectRaw.TabIndex = 3
+        Me.btnSelectRaw.Text = "Select Raw Data"
+        Me.btnSelectRaw.UseVisualStyleBackColor = True
+        '
+        'btnSelectReference
+        '
+        Me.btnSelectReference.Location = New System.Drawing.Point(383, 69)
+        Me.btnSelectReference.Name = "btnSelectReference"
+        Me.btnSelectReference.Size = New System.Drawing.Size(143, 23)
+        Me.btnSelectReference.TabIndex = 4
+        Me.btnSelectReference.Text = "Select Reference Table"
+        Me.btnSelectReference.UseVisualStyleBackColor = True
+        '
+        'btnCreatePivot
+        '
+        Me.btnCreatePivot.Location = New System.Drawing.Point(152, 97)
+        Me.btnCreatePivot.Name = "btnCreatePivot"
+        Me.btnCreatePivot.Size = New System.Drawing.Size(143, 23)
+        Me.btnCreatePivot.TabIndex = 5
+        Me.btnCreatePivot.Text = "Create Pivot Table"
+        Me.btnCreatePivot.UseVisualStyleBackColor = True
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.Image = CType(resources.GetObject("PictureBox1.Image"), System.Drawing.Image)
+        Me.PictureBox1.Location = New System.Drawing.Point(7, 173)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(69, 20)
+        Me.PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom
+        Me.PictureBox1.TabIndex = 6
+        Me.PictureBox1.TabStop = False
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(800, 450)
-        Me.Text = "Form1"
+        Me.ClientSize = New System.Drawing.Size(557, 198)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.Controls.Add(Me.btnCreatePivot)
+        Me.Controls.Add(Me.btnSelectReference)
+        Me.Controls.Add(Me.btnSelectRaw)
+        Me.Controls.Add(Me.txtReferenceData)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.txtRawData)
+        Me.Name = "Form1"
+        Me.Text = "Pivot Table Creator"
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
     End Sub
 
+    Friend WithEvents txtRawData As TextBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents txtReferenceData As TextBox
+    Friend WithEvents btnSelectRaw As Button
+    Friend WithEvents btnSelectReference As Button
+    Friend WithEvents btnCreatePivot As Button
+    Friend WithEvents PictureBox1 As PictureBox
 End Class
--- pivotExelApp/pivotExelApp/Form1.vb
+++ pivotExelApp/pivotExelApp/Form1.vb
@@ -1,3 +1,293 @@
-﻿Public Class Form1
+﻿Imports Microsoft.Office.Interop
 
+Public Class Form1
+
+    Private fileNameReference As String
+    Private pathReference As String
+    Private referencePath As String
+#Region "Buttons"
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles btnSelectRaw.Click
+        Using dialog As New OpenFileDialog
+            If dialog.ShowDialog() <> DialogResult.OK Then Return
+            txtRawData.Text = dialog.FileName
+        End Using
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles btnSelectReference.Click
+        Using dialog As New OpenFileDialog
+            If dialog.ShowDialog() <> DialogResult.OK Then Return
+            txtReferenceData.Text = dialog.FileName
+            fileNameReference = System.IO.Path.GetFileName(dialog.FileName)
+            pathReference = System.IO.Path.GetDirectoryName(dialog.FileName)
+
+            referencePath = pathReference + "\" + "[" + fileNameReference + "]"
+        End Using
+    End Sub
+#End Region
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles btnCreatePivot.Click
+        Dim xlApp As Excel.Application
+        Dim xlWorkBook As Excel.Workbook
+        Dim xlWorkSheet As Excel.Worksheet
+
+        Dim newWorkBook As Excel.Workbook
+        Dim newWorkSheet As Excel.Worksheet
+
+        xlApp = New Excel.Application
+
+        Dim filenameRaw As String = txtRawData.Text
+        If txtRawData.Text.Length > 0 Then
+            filenameRaw = txtRawData.Text
+        Else
+            MsgBox("Please select raw File")
+            Exit Sub
+        End If
+
+        Dim filenameReference As String = txtReferenceData.Text
+        If txtReferenceData.Text.Length > 0 Then
+            filenameReference = txtReferenceData.Text
+        Else
+            MsgBox("Please select reference File")
+            Exit Sub
+        End If
+
+        xlWorkBook = xlApp.Workbooks.Open(filenameRaw)
+        xlWorkSheet = xlWorkBook.Worksheets(1)
+
+        Dim xllastcell As String
+        Dim splt As String()
+
+        xllastcell = xlWorkSheet.Cells.SpecialCells(Excel.XlCellType.xlCellTypeLastCell).Address
+        splt = xllastcell.Split("$")
+
+        'Add new Workbook
+        newWorkBook = xlApp.Workbooks.Add
+        newWorkSheet = newWorkBook.Worksheets("Sheet1")
+
+        xlWorkSheet.Copy(newWorkSheet)
+        'xlApp.Visible = True
+
+        newWorkSheet = newWorkBook.Worksheets(1)
+        newWorkSheet.Activate()
+
+        With newWorkSheet
+            'devicion
+            .Columns("E:E").Insert(Microsoft.Office.Interop.Excel.Constants.xlLeft)
+            .Cells(1, 5).value = "Division"
+            .Cells(2, 5).value = "=VLOOKUP(D2,'" + referencePath + "Division'!$A$1:$B$13,2,)"
+            .Cells(2, 5).Copy()
+            .Range("E3:E" + splt(2)).PasteSpecial(Excel.XlPasteType.xlPasteFormulasAndNumberFormats)
+            'Creditor Name
+            .Columns("I:I").Insert(Microsoft.Office.Interop.Excel.Constants.xlLeft)
+            .Cells(1, 9).value = "Creditor Name"
+            .Cells(2, 9).value = "=VLOOKUP(H2,'" + referencePath + "Insurer'!$A$1:$B$152,2)"
+            .Cells(2, 9).Copy()
+            .Range("I3:I" + splt(2)).PasteSpecial(Excel.XlPasteType.xlPasteFormulasAndNumberFormats)
+            'Policy Class Name 
+            .Columns("N:N").Insert(Microsoft.Office.Interop.Excel.Constants.xlLeft)
+            .Cells(1, 14).value = "Policy Class Name"
+            .Cells(2, 14).value = "=CONCATENATE(M2,""-"",VLOOKUP(M2,'" + referencePath + "PolClass'!$A$2:$B$66,2))"
+            .Cells(2, 14).Copy()
+            .Range("N3:N" + splt(2)).PasteSpecial(Excel.XlPasteType.xlPasteFormulasAndNumberFormats)
+            'Howden 
+            .Columns("O:O").Insert(Microsoft.Office.Interop.Excel.Constants.xlLeft)
+            .Cells(1, 15).value = "Howden Class"
+            .Cells(2, 15).value = "=VLOOKUP(M2,'" + referencePath + "PolClass'!$A$2:$B$66,2)"
+            .Cells(2, 15).Copy()
+            .Range("O3:O" + splt(2)).PasteSpecial(Excel.XlPasteType.xlPasteFormulasAndNumberFormats)
+            'Invoice Class
+            .Columns("AT:AT").Insert(Microsoft.Office.Interop.Excel.Constants.xlLeft)
+            .Cells(1, 46).value = "Invoice Class"
+            .Cells(2, 46).value = "=VLOOKUP(AS2,'" + referencePath + "InvoiceClass'!$A$2:$B$7,2)"
+            .Cells(2, 46).Copy()
+            .Range("AT3:AT" + splt(2)).PasteSpecial(Excel.XlPasteType.xlPasteFormulasAndNumberFormats)
+
+
+
+        End With
+
+        xlWorkBook.Close()
+
+        'start for pivot part
+        CreatePivot(xlApp, newWorkBook, newWorkSheet)
+
+    End Sub
+
+
+
+    'create pivot
+    Private Sub CreatePivot(ByVal xlApp As Excel.Application, ByVal xlWorkBook As Excel.Workbook, ByVal xlWorkSheet As Excel.Worksheet)
+        Dim xlWorkBookPivot As Excel.Workbook
+
+        Dim xllastcell As String
+        xllastcell = xlWorkSheet.Cells.SpecialCells(Excel.XlCellType.xlCellTypeLastCell).Address
+
+        xlWorkBookPivot = xlApp.Workbooks.Add()
+
+        With xlWorkBookPivot
+
+            'a.	Per Line Net Comm
+            .Sheets.Add().name = "Per Line Net Comm"
+            .ActiveSheet.PivotTableWizard(Excel.XlPivotTableSourceType.xlDatabase, xlWorkSheet.Range("A1:" & xllastcell))
+            'filter
+            .ActiveSheet.PivotTables(1).PivotFields("Financial Period").Orientation = Excel.XlPivotFieldOrientation.xlPageField
+            .ActiveSheet.PivotTables(1).PivotFields("Division").Orientation = Excel.XlPivotFieldOrientation.xlPageField
+            .ActiveSheet.PivotTables(1).PivotFields("Invoice Class").Orientation = Excel.XlPivotFieldOrientation.xlPageField
+            'row
+            .ActiveSheet.PivotTables(1).PivotFields("Howden Class").Orientation = Excel.XlPivotFieldOrientation.xlRowField
+            'value
+            With .ActiveSheet.PivotTables(1).PivotFields("Nett Commission")
+                .Orientation = Excel.XlPivotFieldOrientation.xlDataField
+                .Caption = "[Sum of Nett Commission]"
+                .Function = Excel.XlConsolidationFunction.xlSum
+            End With
+
+            'b.	Mgt Report 2
+            .Sheets.Add().name = "Mgt Report 2"
+            .ActiveSheet.PivotTableWizard(Excel.XlPivotTableSourceType.xlDatabase, xlWorkSheet.Range("A1:" & xllastcell))
+            'Filter
+            .ActiveSheet.PivotTables(1).PivotFields("Financial Period").Orientation = Excel.XlPivotFieldOrientation.xlPageField
+            .ActiveSheet.PivotTables(1).PivotFields("Division").Orientation = Excel.XlPivotFieldOrientation.xlPageField
+            .ActiveSheet.PivotTables(1).PivotFields("Executive").Orientation = Excel.XlPivotFieldOrientation.xlPageField
+            .ActiveSheet.PivotTables(1).PivotFields("Invoice Class").Orientation = Excel.XlPivotFieldOrientation.xlPageField
+            'Row
+            .ActiveSheet.PivotTables(1).PivotFields("Client Name").Orientation = Excel.XlPivotFieldOrientation.xlRowField
+            'Value
+            With .ActiveSheet.PivotTables(1).PivotFields("Nett Commission")
+                .Orientation = Excel.XlPivotFieldOrientation.xlDataField
+                .Caption = "[Sum of Nett Commission]"
+                .Function = Excel.XlConsolidationFunction.xlSum
+            End With
+
+            'b.	Mgt Report 1
+            .Sheets.Add().name = "Mgt Report 1"
+            .ActiveSheet.PivotTableWizard(Excel.XlPivotTableSourceType.xlDatabase, xlWorkSheet.Range("A1:" & xllastcell))
+            'Filter
+            .ActiveSheet.PivotTables(1).PivotFields("Financial Period").Orientation = Excel.XlPivotFieldOrientation.xlPageField
+            .ActiveSheet.PivotTables(1).PivotFields("Division").Orientation = Excel.XlPivotFieldOrientation.xlPageField
+            'Row
+            .ActiveSheet.PivotTables(1).PivotFields("Executive").Orientation = Excel.XlPivotFieldOrientation.xlRowField
+            'Value
+            With .ActiveSheet.PivotTables(1).PivotFields("Nett Commission")
+                .Orientation = Excel.XlPivotFieldOrientation.xlDataField
+                .Caption = "[Sum of Nett Commission]"
+                .Function = Excel.XlConsolidationFunction.xlSum
+            End With
+
+            'd.	AE New-Renew
+            .Sheets.Add().name = "AE New-Renew"
+            .ActiveSheet.PivotTableWizard(Excel.XlPivotTableSourceType.xlDatabase, xlWorkSheet.Range("A1:" & xllastcell))
+            'Filter
+            .ActiveSheet.PivotTables(1).PivotFields("Financial Period").Orientation = Excel.XlPivotFieldOrientation.xlPageField
+            .ActiveSheet.PivotTables(1).PivotFields("Division").Orientation = Excel.XlPivotFieldOrientation.xlPageField
+            'Row
+            .ActiveSheet.PivotTables(1).PivotFields("Executive").Orientation = Excel.XlPivotFieldOrientation.xlRowField
+            'Value
+            With .ActiveSheet.PivotTables(1).PivotFields("Premium")
+                .Orientation = Excel.XlPivotFieldOrientation.xlDataField
+                .Caption = "[Sum of Premium]"
+                .Function = Excel.XlConsolidationFunction.xlSum
+            End With
+            With .ActiveSheet.PivotTables(1).PivotFields("Nett Commission")
+                .Orientation = Excel.XlPivotFieldOrientation.xlDataField
+                .Caption = "[Sum of Nett Commission]"
+                .Function = Excel.XlConsolidationFunction.xlSum
+            End With
+            'column
+            .ActiveSheet.PivotTables(1).PivotFields("Data").Orientation = Excel.XlPivotFieldOrientation.xlColumnField
+
+            'e.	Howden Class
+            .Sheets.Add().name = "Howden Class"
+            .ActiveSheet.PivotTableWizard(Excel.XlPivotTableSourceType.xlDatabase, xlWorkSheet.Range("A1:" & xllastcell))
+            'Filter
+            .ActiveSheet.PivotTables(1).PivotFields("Financial Period").Orientation = Excel.XlPivotFieldOrientation.xlPageField
+            .ActiveSheet.PivotTables(1).PivotFields("Division").Orientation = Excel.XlPivotFieldOrientation.xlPageField
+            .ActiveSheet.PivotTables(1).PivotFields("Invoice Class").Orientation = Excel.XlPivotFieldOrientation.xlPageField
+            'Row
+            .ActiveSheet.PivotTables(1).PivotFields("Howden Class").Orientation = Excel.XlPivotFieldOrientation.xlRowField
+            'Value
+            With .ActiveSheet.PivotTables(1).PivotFields("Premium")
+                .Orientation = Excel.XlPivotFieldOrientation.xlDataField
+                .Caption = "[Sum of Premium]"
+                .Function = Excel.XlConsolidationFunction.xlSum
+            End With
+            With .ActiveSheet.PivotTables(1).PivotFields("Nett Commission")
+                .Orientation = Excel.XlPivotFieldOrientation.xlDataField
+                .Caption = "[Sum of Nett Commission]"
+                .Function = Excel.XlConsolidationFunction.xlSum
+            End With
+            'column
+            .ActiveSheet.PivotTables(1).PivotFields("Data").Orientation = Excel.XlPivotFieldOrientation.xlColumnField
+
+            'f.	Invoice Class
+            .Sheets.Add().name = "Invoice Class"
+            .ActiveSheet.PivotTableWizard(Excel.XlPivotTableSourceType.xlDatabase, xlWorkSheet.Range("A1:" & xllastcell))
+            'Filter
+            .ActiveSheet.PivotTables(1).PivotFields("Financial Period").Orientation = Excel.XlPivotFieldOrientation.xlPageField
+            .ActiveSheet.PivotTables(1).PivotFields("Invoice Class").Orientation = Excel.XlPivotFieldOrientation.xlPageField
+            'Row
+            .ActiveSheet.PivotTables(1).PivotFields("Client Name").Orientation = Excel.XlPivotFieldOrientation.xlRowField
+            'Value
+            With .ActiveSheet.PivotTables(1).PivotFields("Premium")
+                .Orientation = Excel.XlPivotFieldOrientation.xlDataField
+                .Caption = "[Sum of Premium]"
+                .Function = Excel.XlConsolidationFunction.xlSum
+            End With
+            With .ActiveSheet.PivotTables(1).PivotFields("Nett Commission")
+                .Orientation = Excel.XlPivotFieldOrientation.xlDataField
+                .Caption = "[Sum of Nett Commission]"
+                .Function = Excel.XlConsolidationFunction.xlSum
+            End With
+            'column
+            .ActiveSheet.PivotTables(1).PivotFields("Data").Orientation = Excel.XlPivotFieldOrientation.xlColumnField
+
+            'g.	Top Client
+            .Sheets.Add().name = "Top Client"
+            .ActiveSheet.PivotTableWizard(Excel.XlPivotTableSourceType.xlDatabase, xlWorkSheet.Range("A1:" & xllastcell))
+            'Filter
+            .ActiveSheet.PivotTables(1).PivotFields("Financial Period").Orientation = Excel.XlPivotFieldOrientation.xlPageField
+            .ActiveSheet.PivotTables(1).PivotFields("Division").Orientation = Excel.XlPivotFieldOrientation.xlPageField
+            'Row
+            .ActiveSheet.PivotTables(1).PivotFields("Client Name").Orientation = Excel.XlPivotFieldOrientation.xlRowField
+            'Value
+            With .ActiveSheet.PivotTables(1).PivotFields("Premium")
+                .Orientation = Excel.XlPivotFieldOrientation.xlDataField
+                .Caption = "[Sum of Premium]"
+                .Function = Excel.XlConsolidationFunction.xlSum
+            End With
+            With .ActiveSheet.PivotTables(1).PivotFields("Nett Commission")
+                .Orientation = Excel.XlPivotFieldOrientation.xlDataField
+                .Caption = "[Sum of Nett Commission]"
+                .Function = Excel.XlConsolidationFunction.xlSum
+            End With
+            'column
+            .ActiveSheet.PivotTables(1).PivotFields("Data").Orientation = Excel.XlPivotFieldOrientation.xlColumnField
+
+            'h.	Top Insurer
+            .Sheets.Add().name = "Top Insurer"
+            .ActiveSheet.PivotTableWizard(Excel.XlPivotTableSourceType.xlDatabase, xlWorkSheet.Range("A1:" & xllastcell))
+            'Filter
+            .ActiveSheet.PivotTables(1).PivotFields("Financial Period").Orientation = Excel.XlPivotFieldOrientation.xlPageField
+            .ActiveSheet.PivotTables(1).PivotFields("Division").Orientation = Excel.XlPivotFieldOrientation.xlPageField
+            'Row
+            .ActiveSheet.PivotTables(1).PivotFields("Creditor Name").Orientation = Excel.XlPivotFieldOrientation.xlRowField
+            'Value
+            With .ActiveSheet.PivotTables(1).PivotFields("Premium")
+                .Orientation = Excel.XlPivotFieldOrientation.xlDataField
+                .Caption = "[Sum of Premium]"
+                .Function = Excel.XlConsolidationFunction.xlSum
+            End With
+            With .ActiveSheet.PivotTables(1).PivotFields("Nett Commission")
+                .Orientation = Excel.XlPivotFieldOrientation.xlDataField
+                .Caption = "[Sum of Nett Commission]"
+                .Function = Excel.XlConsolidationFunction.xlSum
+            End With
+            'column
+            .ActiveSheet.PivotTables(1).PivotFields("Data").Orientation = Excel.XlPivotFieldOrientation.xlColumnField
+
+        End With
+        xlWorkBook.Close(False)
+        xlApp.Visible = True
+
+
+    End Sub
 End Class
